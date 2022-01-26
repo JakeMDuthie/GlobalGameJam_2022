c4df5dfa66ada2e951999eaa73ed492c1c0903cc
Jake <jakeduthie@rocketmail.com>
2022-01-26 19:42:43 +0000

    Sample scene showing double controls

---
+++ GlobalGameJam2022/Assets/Scripts/Character.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float SpeedModifier = 20.0f;
+    public float JumpModifier = 50.0f;
+    public float GravityDirection = 1.0f;
+    
+    private Rigidbody2D _rigidbody2D;
+    private bool _canJump = false;
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        _rigidbody2D.gravityScale = GravityDirection;
+    }
+
+    public void ApplyMovement(float force)
+    {
+        _rigidbody2D.velocity = new Vector2(force*SpeedModifier, _rigidbody2D.velocity.y);
+    }
+
+    public void TryJump()
+    {
+        /*if (!_canJump)
+        {
+            return;
+        }*/
+
+        var JumpForce = (GravityDirection < 0.0f)? (-JumpModifier): JumpModifier;
+
+        JumpForce *= _rigidbody2D.mass;
+        
+        _rigidbody2D.AddForce(new Vector2(0.0f, JumpForce), ForceMode2D.Impulse);
+    }
+}
---
+++ GlobalGameJam2022/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public List<Character> m_Characters = new List<Character>();
+
+    void Update()
+    {
+        // get all inputs for controller first
+        float translation = Input.GetAxis("Horizontal");
+        bool tryJump = Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow);
+
+        // apply all inputs to both characters
+        foreach (var character in m_Characters)
+        {
+            Debug.Log(translation);
+            character.ApplyMovement(translation);
+            if (tryJump)
+            {
+                character.TryJump();
+            }
+        }
+    }
+}
