d99857c1780d5a6bcd7152b8ab2a25259bd3709d
jesus.cruz <jesus.cruz@outplay.com>
2022-01-27 12:03:56 +0000

    [add] select material based on type

--- GlobalGameJam2022/Assets/Scripts/Collectable.cs
+++ GlobalGameJam2022/Assets/Scripts/Collectable.cs
@@ -6,12 +6,17 @@ public class Collectable : MonoBehaviour
 {
 
     public WorldEnum world;
+    public Material greenMaterial;
+    public Material orangeMaterial;
 
     void Start()
     {
-
         var collider = gameObject.AddComponent<CircleCollider2D>();
         collider.isTrigger = true;
+
+        GetComponent<Renderer>().material = world == WorldEnum.Green
+            ? greenMaterial
+            : orangeMaterial;
     }
 
     public void OnTriggerEnter2D(Collider2D other)
