1783257d3919847658c6b6823e43768e6eaef4a6
Jake <jakeduthie@rocketmail.com>
2022-01-27 10:44:56 +0000

    Game Controller and level reset added

---
+++ GlobalGameJam2022/Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public string LevelOne = "TestScene";
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);    
+    }
+
+    private void Start()
+    {
+        SceneManager.LoadScene(LevelOne);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ResetScene();
+        }
+    }
+
+    void ResetScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
