bc43a24fe734d7a8a6c2a9ccb49f03bed3aa75cb
Jake <jakeduthie@rocketmail.com>
2022-01-27 16:10:03 +0000

    Committed WIP UI work

---
+++ GlobalGameJam2022/Assets/Scripts/LevelController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This class controls the general flow of the level as well as calling the game controller to update to the next level
+public class LevelController : MonoBehaviour
+{
+    private GameController GameController;
+    
+    private void Start()
+    {
+        GameController = FindObjectOfType<GameController>();
+    }
+    
+    
+}
---
+++ GlobalGameJam2022/Assets/Scripts/LevelHUD.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LevelHUD : MonoBehaviour
+{
+    public TextMeshProUGUI GreenCounter;
+    public TextMeshProUGUI OrangeCounter;
+
+    private CharacterController _characterController;
+
+    private void Start()
+    {
+        _characterController = FindObjectOfType<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if (_characterController != null)
+        {
+            OrangeCounter.
+        }
+    }
+}
